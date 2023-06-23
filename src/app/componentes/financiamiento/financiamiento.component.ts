d24caa3193eba86336407b0cc485fdae663a8931
Lucas Cortes <luc.cortes@duocuc.cl>
2023-06-22 21:14:20 -0400

    financiamiento listo

--- src/app/componentes/financiamiento/financiamiento.component.ts
+++ src/app/componentes/financiamiento/financiamiento.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, inject} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Firestore, collection, collectionData, deleteDoc, doc, getDocs} from '@angular/fire/firestore';
 import {Chart, registerables} from 'node_modules/chart.js';
 import {UsuariosService} from 'src/app/servicios/usuarios.service';
@@ -7,6 +7,13 @@ Chart.register(...registerables);
 interface Financiamiento {
   Tipo: string;
   Valor: number;
+}
+interface Ingreso {
+  correo: string;
+  valor: number;
+  fecha: string;
+  tipo: string;
+  procedencia: string;
 }
 @Component({
   selector: 'app-financiamiento',
@@ -19,7 +26,10 @@ export class FinanciamientoComponent implements OnInit {
   public ingresos: number = 0;
   public egresos: number = 0;
   public total: number = 0;
+  financiamiento$: Observable<any>;
   userRole!: string;
+  ingresostotales$!: Observable<any[]>;
+  engresostotales$!: Observable<any[]>;
   constructor(private servicio: UsuariosService) {
     const collectionInstance = collection(this.firestore, 'Financiamiento');
     collectionData(collectionInstance, {idField: 'id'}).subscribe((response) => {
@@ -39,6 +49,16 @@ export class FinanciamientoComponent implements OnInit {
 
       this.total = this.ingresos - this.egresos;
       this.userRole = localStorage.getItem('user_role') || '';
+    });
+    this.financiamiento$ = collectionData(collectionInstance, {idField: 'id'});
+    this.financiamiento$.subscribe((elementos: any[]) => {
+      const ingresototal = elementos.filter((ingreso: { tipo: string }) => ingreso.tipo === 'Ingreso');
+      this.ingresostotales$ = of(ingresototal); // Convierte el arreglo en un objeto Observable
+      console.log(this.ingresostotales$);
+
+      const engresototal = elementos.filter((egreso: { tipo: string }) => egreso.tipo === 'Egreso');
+      this.engresostotales$ = of(engresototal); // Convierte el arreglo en un objeto Observable
+      console.log(this.engresostotales$);
     });
   }
 
